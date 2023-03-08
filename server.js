a5d814dde5d9119367766108e7da6272b03a9c02
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-03-08 11:51:22 +0100

    Added cors to the server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDb = require('./DATABASE/database')
+const cors = require('cors')
 
 const app = express()
+dotenv.config()
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-dotenv.config()
 
 connectDb()
 
