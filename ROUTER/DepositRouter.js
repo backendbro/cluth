74f379965422380892def549b6e44ee68da71c6c
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-03-08 22:00:30 +0100

    Remove the protect middleware temporarily

--- ROUTER/DepositRouter.js
+++ ROUTER/DepositRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const DepositService = require('../CONTROLLER/DepositService')
 const {protect, auth} = require('../MIDDLEWARES/protect')
 
-router.use(protect)
+// router.use(protect)
 
 router.post('/get-all', DepositService.getDeposits) 
 router.post("/",  auth("Admin"), DepositService.makeDeposit)
